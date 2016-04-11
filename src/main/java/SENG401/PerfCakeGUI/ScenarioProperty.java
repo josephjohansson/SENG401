bed3dab66652841d50b637c27dfc9d9fd0f5bb91
Tanner Hagel <tshagel@ucalgary.ca>
2016-04-11 15:45:51 -0600

    Added ScenarioProperty class

---
+++ src/main/java/SENG401/PerfCakeGUI/ScenarioProperty.java
@@ -0,0 +1,41 @@
+package SENG401.PerfCakeGUI;
+
+/**
+ * Used to represent the scenario property that will be
+ * used in the generation of the arguments used to start
+ * PerfCake
+ * @author Tanner Hagel
+ *
+ */
+public class ScenarioProperty {
+
+	/**
+	 * The property name (for example, in
+	 * -Dserver.host=www.google.ca the name is 'server.host'
+	 */
+	private String propertyName;
+	
+	/**
+	 * The property value (for example, in
+	 * -Dserver.host=www.google.ca the value is 'www.google.ca'
+	 */
+	private String propertyValue;
+	
+	
+	public String getPropertyName() {
+		return propertyName;
+	}
+	
+	public String getPropertyValue() {
+		return propertyValue;
+	}
+	
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+	
+	public void setPropertyValue(String propertyValue) {
+		this.propertyValue = propertyValue;
+	}
+	
+}
